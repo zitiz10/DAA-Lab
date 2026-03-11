fbf284555ee7952f33bde18d18a7b58f63d3913e
zitiz10 <nitinnegi4262@gmail.com>
2026-03-11 19:10:09 +0530

    Add files via upload

---
+++ first and last occurence .cpp
@@ -0,0 +1,63 @@
+
+using namespace std;
+
+int firstOccurrence(int arr[], int n, int key) {
+    int low = 0, high = n - 1, ans = -1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] == key) {
+            ans = mid;
+            high = mid - 1;
+        } else if (arr[mid] < key) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
+
+int lastOccurrence(int arr[], int n, int key) {
+    int low = 0, high = n - 1, ans = -1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] == key) {
+            ans = mid;
+            low = mid + 1;
+        } else if (arr[mid] < key) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int n;
+        cin >> n;
+
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+
+        int key;
+        cin >> key;
+
+        int first = firstOccurrence(arr, n, key);
+        if (first == -1) {
+            cout << "Key not present" << endl;
+        } else {
+            int last = lastOccurrence(arr, n, key);
+            int count = last - first + 1;
+            cout << key << " " << count << endl;
+        }
+    }
+
+    return 0;
+}
